1c0155018af40ce6a123f66b29435712e1660556
Beréti Zsófia <78954790+dzsofri@users.noreply.github.com>
2022-03-09 13:19:23 +0100

    Add files via upload

--- kepek_png.py
+++ kepek_png.py
@@ -1,13 +1,17 @@
 from tkinter import *
+def klikk():
+    print("Klikkeltem")
 ablak1 =  Tk()
 gyoker = "C:\\Users\\beretizsofia\\Desktop\\python\\"
 
 ablak1.geometry("450x450")
 ablak1.title("IKT Tkinter")
-icon = PhotoImage(file='cica.png')
+icon = PhotoImage(file=gyoker+'cica.png')
 ablak1.iconphoto(True, icon)
 ablak1.config(background='black')
 elsokep = PhotoImage(file=gyoker+"among-us-sus.gif")
+gombkep = PhotoImage(file=gyoker+"among-us-sus.gif")
+
 cimke= Label(ablak1,
              text="Üdvözlet",      
              fg='#553388',
@@ -19,5 +23,20 @@ cimke= Label(ablak1,
              pady=30,
              image=elsokep,
              compound="bottom").pack()
+
+gomb = Button(ablak1,
+              text= "kattints",
+              font= ("Comic Sans", 35, "bold"),
+              bg="yellow",
+              relief=SUNKEN,
+              bd=10,
+              command=klikk,
+              padx=12,
+              pady=12,
+              #image=gombkep,
+              compound="bottom",
+              activebackground="blue",
+              activeforeground="yellow",
+              state=ACTIVE).pack()
 
 ablak1.mainloop()
